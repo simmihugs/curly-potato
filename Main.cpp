c65b48236b325137f4bdb9a66fe2dd8866cb69d3
simmihugs <simon.graetz@gmx.de>
2024-05-20 15:12:48 +0200

    observered undesired behavior in terms of incremtation

--- Main.cpp
+++ Main.cpp
@@ -2,21 +2,21 @@
 #include <thread>
 
 auto main(int argc, char *argv[]) -> int {
-  int global = 10000;
-  std::cout << "global: " << global << "\n";
-  auto func = [global]() mutable {
-    std::cout << "global: " << global << "\n";
-    int tmp = global;
-    tmp += 10000;
-    global = tmp;
-    std::cout << "global: " << global << "\n";
+  int sum = 0;
+  std::cout << "sum = " << sum << "\n";
+
+  auto f = [&sum]() {
+    for (int i = 0; i < 100000; i++) {
+      sum += 1;
+    }
   };
 
-  std::thread t1{func};
-  std::thread t2{func};
+  std::thread t1(f);
+  std::thread t2(f);
   t1.join();
   t2.join();
 
-  std::cout << "global: " << global << "\n";
+  std::cout << "sum = " << sum << "\n";
+
   return 0;
 }
